e0be9e35b6d87611c1576efb4eeb573927d75c71
hasibulislamha01 <hasibulislamha01@gmail.com>
2024-07-04 18:42:28 +0600

    deployment links changed

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ app.use(cors({
   origin:[
     'http://localhost:5000',
     'http://localhost:5173',
-    'https://hasibul-porfolio-server.vercel.app'
+    'https://hasibul-porfolio-server.vercel.app',
+    'https://hasibul-portfolio-dac39.web.app'
   ]
 }))
 // Enable CORS for all origins
