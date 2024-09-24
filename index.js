d4209379848be34ed79517b41f98c1ed2735fafa
hasibulislamha01 <hasibulislamha01@gmail.com>
2024-09-25 00:59:42 +0600

    adding blogs feature added

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 
 
 app.use(cors({
-  origin:[
+  origin: [
     'http://localhost:5000',
     'http://localhost:5173',
     'https://hasibul-porfolio-server.vercel.app',
@@ -44,7 +44,11 @@ async function run() {
 
     const projectsCollection = client.db("Hasibul").collection('Projects');
     const blogsCollection = client.db("Hasibul").collection('Blogs');
+    const skillsCollection = client.db("Hasibul").collection('skillsCollection');
 
+
+
+    // ----------- projects handling api's -------------------
 
     app.post('/projects', async (req, res) => {
       const projectInfo = req.body
@@ -53,19 +57,36 @@ async function run() {
       res.send(result)
     })
 
-    app.get('/projects', async(req, res)=> {
+    app.get('/projects', async (req, res) => {
       const result = await projectsCollection.find().toArray()
       res.send(result)
     })
 
 
-    // bloogs routes
-    app.post('/blogs', async(req, res)=> {
-      const blog = req.body
+    // ------------------ blogs routes ----------------------
+
+    app.post('/blogs', async (req, res) => {
+      const blog = req?.body
       console.log(blog)
+      const result = await blogsCollection.insertOne(blog)
+      res.send(result)
     })
 
 
+    app.get('/blogs', async (req, res) => {
+      const result = await blogsCollection.find().toArray()
+      res.send(result)
+    })
+
+
+
+
+    //--------------------- skills api's -------------------
+
+    app.get('/skills', async (req, res) => {
+      const skills = await skillsCollection.find().toArray()
+      res.send(skills)
+    })
 
 
 
@@ -91,10 +112,10 @@ app.use(express.json())
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello from hasibul!')
 })
 
 
 app.listen(port, () => {
-  console.log(`travel site is running on port ${port}`)
+  console.log(`Hasibul running on port ${port}`)
 })
